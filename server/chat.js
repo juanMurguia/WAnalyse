1f7276e5d012d1283927f2d9400bdaf4185bd0c2
AleFalcone27 <83794890+AleFalcone27@users.noreply.github.com>
2024-03-11 20:41:01 -0300

    Inicializamos el atributo path dentro del constructor de la clase Chat

--- server/chat.js
+++ server/chat.js
@@ -3,11 +3,11 @@ class Chat {
     constructor(name) {
 
         this.name = name;
+        this.path =  `./uploads/ + ${name}`
         this.totalMessages;
         this.user1;
         this.user2;
         this.totalMessagesUser1;
         this.totalMessagesUser2;
-
     }
 }
