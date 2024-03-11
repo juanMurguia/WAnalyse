63a77842aa99ec2a196875be47227142f77da523
AleFalcone27 <83794890+AleFalcone27@users.noreply.github.com>
2024-03-11 20:28:56 -0300

    Creo clase Chat

---
+++ server/chat.js
@@ -0,0 +1,13 @@
+class Chat {
+    
+    constructor(path) {
+
+        this.path = path;
+        this.totalMessages;
+        this.user1;
+        this.user2;
+        this.totalMessagesUser1;
+        this.totalMessagesUser2;
+
+    }
+}
