2a8e30bd85f0b4c85a67b260c2116d515cf6431e
AleFalcone27 <83794890+AleFalcone27@users.noreply.github.com>
2024-03-11 20:02:59 -0300

    Servidor corriendo

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const multer = require('multer')
+const cors = require('cors');
+
+const { contadorDeAlejos } = require("./txtProcesor");
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+const port = process.env.PORT || 3000
+
+
+const upload = multer({ dest: "uploads/" })
+
+app.post("/uploads", upload.single("file") ,(req,res) =>{
+    console.log(req.file)
+    res.send("Termina")
+    contadorDeAlejos( "./uploads/" + req.file.filename)
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port http://localhost:${port}`);
+});
