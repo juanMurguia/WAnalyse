3f9b39d3916373884488c342027020ef8d0e5754
AleFalcone27 <83794890+AleFalcone27@users.noreply.github.com>
2024-03-11 21:22:54 -0300

    Modifico lógica index.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const multer = require('multer')
 const cors = require('cors');
 
-const { contadorDeAlejos } = require("./txtProcessor");
+const { analyse } = require("./chat");
 
 const app = express();
 
